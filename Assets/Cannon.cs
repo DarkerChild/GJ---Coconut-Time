755496b12789e9a6441071de06539c6a40aa4312
Darker <wkc419@hotmail.com>
2020-05-01 23:19:10 +0100

    Demo map layout complete. Struggling to make cannon fire in forward direction!!!

---
+++ Assets/Cannon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField] GameObject cannonBallTemplate = null;
+    [Space]
+    [SerializeField] float cannonFireingForce = 1000f;
+
+    private Vector3 cannonForcePosition;
+
+    private void FixedUpdate()
+    {
+        cannonForcePosition = GetComponentInChildren<ParticleSystem>().transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Firing");
+            FireCannon();
+        }
+    }
+
+    private void FireCannon()
+    {
+        GameObject cannonBall = Instantiate(cannonBallTemplate, transform);
+        CannonBall cannonBallScript = cannonBall.GetComponent<CannonBall>();
+        cannonBallScript.cannonBallFiringForce = cannonFireingForce;
+        cannonBall.SetActive(true);
+        Destroy(cannonBall, 2f);
+    }
+}
---
+++ Assets/CannonBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+    new Rigidbody rigidbody;
+
+    public float cannonBallFiringForce = 10f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        print(transform.rotation.eulerAngles);
+        Vector3 firingDirection = Quaternion.Euler(transform.rotation.eulerAngles) * Vector3.forward;
+        rigidbody.AddForce(firingDirection * cannonBallFiringForce);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
